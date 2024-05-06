6d86a1bd59b8c793b590edb8fbb541def950c967
Albus9000 <157077551+Albus9000@users.noreply.github.com>
2024-05-06 10:42:27 -0400

    alert from lambda

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import {Cookie} from "./components/Cookie";
-
+import axios from "axios";
 function App() {
+  useEffect(() => {
+    axios.get("https://6zlk74lrhsnaskh5ufoyjj7vly0nkhip.lambda-url.us-east-1.on.aws/").then((res) => {
+      alert(res);
+    });
+  }, []);
   return (
     <div className="App">
       <Cookie />
