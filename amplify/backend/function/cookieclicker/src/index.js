87fcbf7f1b06d5aeb354cc9bd7696a156dfb429e
Danny Forest <ford86@gmail.com>
2024-05-06 11:03:07 -0400

    Calling backend

---
+++ amplify/backend/function/cookieclicker/src/index.js
@@ -0,0 +1,21 @@
+
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    const cookies = [
+        "cookie", "oreo-cookie", "chocolate-cookie"
+    ]
+
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+        body: JSON.stringify(cookies),
+    };
+};
