bcef7a0f292467958f50ff51cbaba2c4a440c009
Audrey Laurin <1450217@cshawi.ca>
2024-05-07 13:25:24 -0400

    backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import {Cookie} from "./components/Cookie";
 import styled from 'styled-components';
-
-const cookies = [
-    "cookie", "oreo-cookie", "chocolate-cookie"
-]
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-        {/*<div style={styles.cookiesContainer}>*/}
-        <CookiesContainer2>
-            {
-                cookies.map(cookie => (
-                    <Cookie image={cookie + '.webp'} />
-                ))
-            }
-        </CookiesContainer2>
-    </div>
-  );
+
+    const [cookies, setCookies] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://fgn7m2uwzun4lplwsdg74s74wq0pwugh.lambda-url.us-east-1.on.aws/')
+            .then(res => {
+                setCookies(res.data);
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            {/*<div style={styles.cookiesContainer}>*/}
+            <CookiesContainer2>
+                {
+                    cookies.map(cookie => (
+                        <Cookie image={cookie + '.webp'}/>
+                    ))
+                }
+            </CookiesContainer2>
+        </div>
+    );
 }
 
 export default App;
 
 const CookiesContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
 `;
 
 const CookiesContainer2 = styled(CookiesContainer)`
-  background-color: lightblue;
+    background-color: lightblue;
 `;
 
 // const styles = {
