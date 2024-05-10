4ac70ad93d1e558d4c9afa6a6f718c1f4b81498f
Danny Forest <ford86@gmail.com>
2024-05-10 10:07:01 -0400

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Cookie} from "./components/Cookie";
 import styled from 'styled-components';
 import Leaderboard from "./components/Leaderboard";
-import { Datastore } from 'aws-sdk/@datastore';
+
+import {Amplify} from 'aws-amplify';
+import {DataStore} from '@aws-amplify/datastore';
+import config from './amplifyconfiguration.json';
+import {UserScore} from "./models";
+
+Amplify.configure(config);
 
 
 const cookies = [
@@ -11,31 +17,40 @@ const cookies = [
 ]
 
 function App() {
-  return (
-    <div className="App">
-        {/*<div style={styles.cookiesContainer}>*/}
-        <CookiesContainer2>
-            {
-                cookies.map(cookie => (
-                    <Cookie image={cookie + '.webp'} />
-                ))
-            }
-        </CookiesContainer2>
-        <Leaderboard />
-    </div>
-  );
+    const [userScores, setUserScores] = useState<UserScore[]>([]);
+    useEffect(() => {
+        console.log('test')
+        DataStore.query(UserScore).then(userScores => {
+            console.log('here')
+            console.log(userScores);
+        })
+    }, []);
+
+    return (
+        <div className="App">
+            {/*<div style={styles.cookiesContainer}>*/}
+            <CookiesContainer2>
+                {
+                    cookies.map(cookie => (
+                        <Cookie key={cookie} image={cookie + '.webp'}/>
+                    ))
+                }
+            </CookiesContainer2>
+            <Leaderboard/>
+        </div>
+    );
 }
 
 export default App;
 
 const CookiesContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
 `;
 
 const CookiesContainer2 = styled(CookiesContainer)`
-  background-color: lightblue;
+    background-color: lightblue;
 `;
 
 // const styles = {
