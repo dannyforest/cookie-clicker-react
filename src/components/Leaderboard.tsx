2ceca7a53e5068d7d606620f6a1f5d75c59e463d
Danny Forest <ford86@gmail.com>
2024-05-10 10:12:10 -0400

    WIP

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,6 +1,10 @@
 import {useEffect, useState} from "react";
+import {Amplify} from 'aws-amplify';
+import {DataStore} from '@aws-amplify/datastore';
+import config from '../amplifyconfiguration.json';
 import {UserScore} from "../models";
-import {DataStore} from "@aws-amplify/datastore";
+
+Amplify.configure(config);
 
 const Leaderboard = () => {
     const [userScores, setUserScores] = useState<UserScore[]>([]);
@@ -9,8 +13,8 @@ const Leaderboard = () => {
             console.log('test');
             try {
                 const userScores = await DataStore.query(UserScore);
-                setUserScores(userScores);
                 console.log(userScores);
+                setUserScores(userScores);
             } catch (error) {
                 console.log('Error retrieving posts', error);
             }
@@ -22,6 +26,11 @@ const Leaderboard = () => {
     return (
         <div>
             <h1>Leaderboard</h1>
+            <ul>
+                {userScores.map((userScore) => (
+                    <li key={userScore.id}>{userScore.name} - {userScore.score}</li>
+                ))}
+            </ul>
         </div>
     )
 }
