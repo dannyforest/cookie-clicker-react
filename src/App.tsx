8d7d3f935471ab5d49a7435323f388e2fbb8828a
Albus9000 <157077551+Albus9000@users.noreply.github.com>
2024-05-13 09:06:54 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,66 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Cookie} from "./components/Cookie";
-import axios from "axios";
+import styled from 'styled-components';
+import Leaderboard from "./components/Leaderboard";
+
+import {Amplify} from 'aws-amplify';
+import {DataStore} from '@aws-amplify/datastore';
+import config from './amplifyconfiguration.json';
+import {UserScore} from "./models";
+
+Amplify.configure(config);
+
+
+const cookies = [
+    "cookie", "oreo-cookie", "chocolate-cookie"
+]
+
 function App() {
-  useEffect(() => {
-    axios.get("https://6zlk74lrhsnaskh5ufoyjj7vly0nkhip.lambda-url.us-east-1.on.aws/").then((res) => {
-      alert(res.data);
+    const [username, setUsername] = useState(() => {
+        const savedUsername = localStorage.getItem('username');
+        return savedUsername !== null ? savedUsername : '';
     });
-  }, []);
-  return (
-    <div className="App">
-      <Cookie />
-    </div>
-  );
+
+    const handleUsernameChange = (event: any) => {
+        setUsername(event.target.value);
+    };
+
+    return (
+        <div className="App">
+            {/*<div style={styles.cookiesContainer}>*/}
+            <label htmlFor="username">Username:</label>
+            <input
+                type="text"
+                id="username"
+                name="username"
+                value={username}
+                onChange={handleUsernameChange}
+            />
+            <CookiesContainer2>
+                {
+                    cookies.map(cookie => (
+                        <Cookie key={cookie} image={cookie + '.webp'} username={username} />
+                    ))
+                }
+            </CookiesContainer2>
+            <Leaderboard/>
+        </div>
+    );
 }
 
 export default App;
+
+const CookiesContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+`;
+
+const CookiesContainer2 = styled(CookiesContainer)`
+    background-color: lightblue;
+`;
+
+// const styles = {
+//     cookiesContainer: {display: 'flex', flexDirection: 'row', justifyContent: 'center'}
+// }
--- src/components/Cookie.tsx
+++ src/components/Cookie.tsx
@@ -1,41 +1,69 @@
-import React, { useState, useEffect, MouseEventHandler } from 'react';
+import React, {useState, useEffect} from 'react';
+import {DataStore} from "@aws-amplify/datastore";
+import {UserScore} from "../models";
 
-export const Cookie = () => {
+interface Props {
+    image: string;
+    username: string;
+}
+
+export const Cookie = ({image, username}: Props) => {
+    const localStorageKey = `cookieCounter${image}`;
+
     // Initialize the counter from localStorage if available, otherwise start at 0
-    const [counter, setCounter] = useState(parseInt(localStorage.getItem('cookieCounter')??'0'));
+    const [counter, setCounter] = useState(() => {
+        const savedCounter = localStorage.getItem(localStorageKey);
+        return savedCounter !== null ? parseInt(savedCounter, 10) : 0;
+    });
+
+
+    const [img, setImg] = useState(image);
 
     // Function to handle the click event that increments the counter
     const handleClick = () => {
-        setCounter(counter+1);
-    }
-    const handleRightClick = (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
-        event.preventDefault()
-        setCounter(counter-1);
+        setCounter(prevCounter => prevCounter + 1);
     }
 
     // Function to reset the counter
     const handleReset = () => {
+        // setImg('oreo-cookie.webp');
         setCounter(0);
+    }
+
+    const addOrUpdateUserScore = async () => {
+        // Add or update the user score in the database
+        const original = await DataStore.query(UserScore, (c) => c.name.eq(username));
+
+        if (original.length > 0) {
+            if (original[0].score < counter) {
+                const updatedUserScore = await DataStore.save(
+                    UserScore.copyOf(original[0], updated => {
+                        updated.score = counter
+                    })
+                );
+
+                console.log(updatedUserScore);
+            }
+        } else {
+            await DataStore.save(new UserScore({name: username, score: counter}));
+        }
     }
 
     // Use useEffect to store the counter in localStorage when it changes
     useEffect(() => {
-        localStorage.setItem('cookieCounter', String(counter));
+        localStorage.setItem(localStorageKey, String(counter));
+        addOrUpdateUserScore();
     }, [counter]);
+
     return (
-        <>
-            <img src='cookie.webp' alt='a very delicious looking cookie! Yummy yummy!'
-                onClick={handleClick} // Add the onClick handler to the image
-                onContextMenu={(event) => { handleRightClick(event) }}
-                style={
-                    {
-                        width: 400 + counter * 3,
-                        height: 400 + counter * 3,
-                    }
-                }
+        <div className={'cookie-container'}>
+            <h1>{image}</h1>
+            {/*<img src={img} alt={'a very delicious looking cookie! Yummy yummy!'}*/}
+            <img src={image} alt={'a very delicious looking cookie! Yummy yummy!'}
+                 onClick={handleClick} // Add the onClick handler to the image
             />
-            <h1>Cookie clicks: {counter}</h1>
+            <h1>Cookie clicks: {counter}</h1> {/* Display the current counter */}
             <button onClick={handleReset}>Reset Counter</button>
-        </>
+        </div>
     )
 }
