773bb1a9ee84143cd5fa77b715a497193ab01bd5
Albus9000 <157077551+Albus9000@users.noreply.github.com>
2024-05-22 10:32:23 -0400

    test

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,22 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "cookieclicker": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "cookieclickerf91a4194": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "function": {
     "cookieclicker": {
       "Arn": "string",
--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,55 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import {Cookie} from "./components/Cookie";
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Cookie } from "./components/Cookie";
+import styled from "styled-components";
 import Leaderboard from "./components/Leaderboard";
 
-import {Amplify} from 'aws-amplify';
-import {DataStore} from '@aws-amplify/datastore';
-import config from './amplifyconfiguration.json';
-import {UserScore} from "./models";
-
+import { Amplify } from "aws-amplify";
+import config from "./amplifyconfiguration.json";
+import { UserScore } from "./models";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 Amplify.configure(config);
 
-
-const cookies = [
-    "cookie", "oreo-cookie", "chocolate-cookie"
-]
+const cookies = ["cookie", "oreo-cookie", "chocolate-cookie"];
 
 function App() {
-    const [username, setUsername] = useState(() => {
-        const savedUsername = localStorage.getItem('username');
-        return savedUsername !== null ? savedUsername : '';
-    });
+	const [username, setUsername] = useState(() => {
+		const savedUsername = localStorage.getItem("username");
+		return savedUsername !== null ? savedUsername : "";
+	});
 
-    const handleUsernameChange = (event: any) => {
-        setUsername(event.target.value);
-    };
+	const handleUsernameChange = (event: any) => {
+		setUsername(event.target.value);
+	};
 
-    return (
-        <div className="App">
-            {/*<div style={styles.cookiesContainer}>*/}
-            <label htmlFor="username">Username:</label>
-            <input
-                type="text"
-                id="username"
-                name="username"
-                value={username}
-                onChange={handleUsernameChange}
-            />
-            <CookiesContainer2>
-                {
-                    cookies.map(cookie => (
-                        <Cookie key={cookie} image={cookie + '.webp'} username={username} />
-                    ))
-                }
-            </CookiesContainer2>
-            <Leaderboard/>
-        </div>
-    );
+	return (
+		<div className="App">
+			<Authenticator>
+				{/*<div style={styles.cookiesContainer}>*/}
+				<label htmlFor="username">Username:</label>
+				<input type="text" id="username" name="username" value={username} onChange={handleUsernameChange} />
+				<CookiesContainer2>
+					{cookies.map((cookie) => (
+						<Cookie key={cookie} image={cookie + ".webp"} username={username} />
+					))}
+				</CookiesContainer2>
+				<Leaderboard />
+			</Authenticator>
+		</div>
+	);
 }
 
 export default App;
 
 const CookiesContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
 `;
 
 const CookiesContainer2 = styled(CookiesContainer)`
-    background-color: lightblue;
+	background-color: lightblue;
 `;
 
 // const styles = {
